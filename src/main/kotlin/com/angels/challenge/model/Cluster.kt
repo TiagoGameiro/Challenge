96ac889c53f016c09ad8bfccdf90bed7b4bbfdad
Tiago Gameiro <tccgameiro@gmail.com>
2021-09-22 15:33:03 +0100

    commit developments

--- src/main/kotlin/com/angels/challenge/model/Cluster.kt
+++
@@ -1,9 +0,0 @@
-package com.angels.challenge.model
-
-import com.angels.challenge.entity.ClusterEntity
-import com.fasterxml.jackson.annotation.JsonProperty
-
-data class Cluster (
-    @JsonProperty("name")
-    val name: String?
-)
